36090fc18056f9935f68b1819efbd840507e330d
Oj Obasi <ericel123new@gmail.com>
2016-09-12 21:27:53 +0900

    Initial Commit of Slango

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name slangoApp
+ * @description
+ * # slangoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('slangoApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name slangoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the slangoApp
+ */
+angular.module('slangoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
